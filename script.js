cbe5a39548f006c095c381306dfe9a451db1bf54
Shubham Jamdade <57455051+SJ-Shubh@users.noreply.github.com>
2025-01-26 01:15:04 +0530

    new method

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+// Basic smooth scrolling for navigation links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
